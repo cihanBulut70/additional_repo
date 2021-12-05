51893e3a9f10e4af6895cbfa17abbccb6d02fe64
Cihan Bulut <bulutcihan1970@gmail.com>
2021-12-05 14:29:27 -0500

    First Commit

---
+++ src/my_git_repo/FirstJavaClass.java
@@ -0,0 +1,16 @@
+package my_git_repo;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+
+		int num=3;
+		int num2=2;
+		
+		System.out.println(num *num2);
+
+
+		
+	}
+
+}
